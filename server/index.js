c6f2b1e6c6c4af1edd8f3c8412da2f3e61d0c19f
Robert Arteaga <robertarteaga@Roberts-iMac.local>
2020-05-26 17:46:27 -0700

    added post and get all todo + dotenv

--- server/index.js
+++ server/index.js
@@ -1,9 +1,41 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const pool = require('./db');
 
 app.use(cors());
 app.use(express.json());
+
+// Routes //
+
+// POST - create a todo
+app.post('/todos', async (req, res) => {
+	try {
+		const { description } = req.body;
+		const newTodo = await pool.query(
+			'INSERT INTO todo (description) VALUES($1) RETURNING *',
+			[description]
+		);
+
+		res.json(newTodo.rows[0]);
+	} catch (err) {
+		console.error(err.message);
+	}
+});
+
+// GET - get todos
+app.get('/todos', async (req, res) => {
+	try {
+		const allTodos = await pool.query('SELECT * FROM todo');
+
+		res.json(allTodos.rows);
+	} catch (err) {
+		console.error(err.message);
+	}
+});
+// PUT - update a todo
+
+// DELETE - delete a todo
 
 app.listen(5000, () => {
 	console.log('server has started on port 5000');
